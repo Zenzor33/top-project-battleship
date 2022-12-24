8461d5c893a6cfd919ff4361edff6abeb01e8bfd
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2022-12-24 07:39:18 -0300

    coordinates includes

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 // Begin your app by creating the Ship factory function
 // Your ‘ships’ will be objects that include their length, the number of times they’ve been hit and whether or not they’ve been sunk.
 
-module.exports.createShip = function (length) {
+export const createShip = (length, coordinates) => {
+  // coordinates expects a 2d array
   return {
     length,
     beenHit: 0,
     sunk: false,
+    coordinates,
     hit() {
-      this.beenHit += 1;
+      return (this.beenHit += 1);
     },
     isSunk() {
-      this.beenHit >= this.length ? true : false;
+      return this.beenHit >= this.length ? true : false;
     },
   };
 };
 
-// module.exports = createShip;
+export const gameBoard = () => {
+  return {
+    recieveAttack(coordinates) {
+      // if (ship1.coordinates.includes(coordinates))
+    },
+  };
+};
+
+/* 
+Gameboards should have a receiveAttack function 
+- that takes a pair of coordinates, 
+- determines whether or not the attack hit a ship and 
+- then sends the ‘hit’ function to the correct ship, or records the coordinates of the missed shot.
+*/
